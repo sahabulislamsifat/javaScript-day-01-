914887aec3106493e4b6bb8a6d342b4ebd563f1a
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-02-26 01:44:59 +0600

    explore Not a Number or NaN

---
+++ nun.js
@@ -0,0 +1,49 @@
+// var x = 5;
+
+// isNaN()
+
+// not a number, and cannot be transformed into number = true
+// number or cannot be transformed into number = false
+
+// var x = "Sifat";
+// var x = 5;
+// var x = "5";
+
+// console.log(isNaN(x));
+
+// var x = 6;
+// var y = 4;
+// var z = 6;
+
+// Assignment Operator (var x = 5);
+
+// Comparison Operator (===, !== , <=,>=,<,>)
+
+// console.log(x === y);
+// console.log(x === z);
+// console.log(x < y);
+// console.log(x > y);
+// console.log(x <= y);
+// console.log(x >= y);
+
+// Difference between === and ==
+
+// == check only values
+// === check values with its data type
+
+// var x = 5;
+// var y = 5;
+// var y = "5";
+// var y = "";
+
+// console.log(x == y);
+// console.log(x === y);
+
+// console.log(y);
+// console.log(!y);
+
+// var x = "Sifat";
+// var x = "";
+
+// console.log(!x);
+// console.log(!!x);
