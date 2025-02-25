7c6f33f1047b595b2dbe4fcd6a5c8accd3c15176
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-02-25 22:58:15 +0600

    explore javascript basic math

---
+++ math.js
@@ -0,0 +1,63 @@
+// console.log(3 + 5);
+
+// Plus
+var x = 3;
+var y = 5;
+var z = 5.52;
+// console.log(x + y + z);
+
+// const total = x + y + z;
+// console.log(total);
+
+// Sub
+// var sub = x - y;
+// console.log(sub);
+
+// multiple
+// var multi = x * y;
+// console.log(multi);
+
+// divide
+// var divide = x / y;
+// console.log(divide);
+
+// var divide = x / z;
+// console.log(divide);
+// console.log(divide.toFixed(4));
+// var value = parseFloat(divide.toFixed(4));
+// var value = parseInt(divide.toFixed(4));
+// var value = divide.toFixed(4);
+
+// console.log(5 + value);
+// console.log(5 + parseFloat(value));
+// console.log(5 + Number(value));
+
+// console.log(10 / 3);
+
+// Multi
+// var x = 3 * 2;
+// var x = 3 ** 2;
+// var x = 3 ** 3;
+// var x = 3 ** 4;
+
+// console.log(x);
+
+// Modules
+
+// console.log(10 / 2);
+// console.log(10 % 2);
+// console.log(10 % 3);
+
+// console.log(10 % 10);
+// console.log(8 % 10);
+
+// Short Hand
+
+var x = 5;
+
+// y = x + 3;
+// y += 3;
+// x = x - 1;
+// x++;
+// x--;
+console.log(x);
