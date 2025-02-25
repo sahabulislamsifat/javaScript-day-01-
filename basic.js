cab6f846e3445770a178e2b2d0e7d658c560e25b
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-02-25 17:55:33 +0600

    basic JavaScript

---
+++ basic.js
@@ -0,0 +1,64 @@
+// console.log(5);
+// console.log("Sifat");
+// console.log(5.2);
+
+// var name = "Sifat";
+// console.log(name);
+
+// var name = 51;
+// console.log(name);
+// var name = 15;
+// console.log(name);
+
+// Data Type : Number,String,Boolean,Undefined,NaN,Null
+
+// var x = 5;
+// var x = "5";
+// var x = true;
+// var x = false;
+// var x = null;
+
+// console.log(typeof x);
+
+// Truthy Value => true, 'hello', number
+// Falsy Value => false, "", 0
+
+// const x = 6;
+// const x = "Hello";
+// const x = true;
+
+// console.log(!!x);
+
+// const x = false;
+// const x = "";
+// const x = 0;
+
+// console.log(!!x);
+
+// Variable Naming Convention
+//  Possible
+
+// var Sifat;
+// var dev_sifat; // Snake Case
+// var devSifat; // Camel Case
+// var DevSifat; // Pascel Case
+// var sifat1;
+
+// Not Possible
+
+// var 554;
+// var 01sifat
+// var sifat-Dev16
+// var @sifat
+
+// Good Practice
+
+// var sifat;
+// var devSifat;
+// var dev_sifat;
+
+// var sifat = "my name";
+// var Sifat = "Your Name";
+
+// console.log(sifat);
+// console.log(Sifat);
